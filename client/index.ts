26d0a6ae496164a0b02f4236f3353c963d873d41
sofia <sofiavign@gmail.com>
2021-08-08 10:18:39 +0200

    probando env

--- client/index.ts
+++ client/index.ts
@@ -1,8 +1,15 @@
-//comentario
 fetch("/hola")
   .then((data) => {
     return data.json();
   })
   .then((ress) => {
     document.querySelector(".root").textContent = JSON.stringify(ress);
+  });
+
+fetch("/env")
+  .then((data) => {
+    return data.json();
+  })
+  .then((ress) => {
+    document.querySelector(".env").textContent = JSON.stringify(ress);
   });
